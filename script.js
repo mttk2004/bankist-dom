06e94e110c0da62fe8f7b2c0c2f887a20435e966
= <=>
2024-06-18 09:29:50 +0700

    v4.0 Menu fade animation

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 'use strict';
 
 ///////////////////////////////////////
-// Modal window
 
 const header = document.querySelector(".header");
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+const nav = document.querySelector(".nav");
+
+////////////////////////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -84,10 +90,6 @@ document.querySelector(".nav__links")
 
 
 // Tabbed components
-const tabs = document.querySelectorAll(".operations__tab");
-const tabsContainer = document.querySelector(".operations__tab-container");
-const tabsContent = document.querySelectorAll(".operations__content");
-
 tabsContainer.addEventListener('click', function(e) {
   const tab = e.target.closest(".operations__tab");
   if (!tab) return;
@@ -105,3 +107,21 @@ tabsContainer.addEventListener('click', function(e) {
       .classList.add('operations__content--active');
   // tabsContent[activeNum-1].classList.add('operations__content--active')
 })
+
+
+// Menu fade animation
+const handleHover = function(e) {
+  if (!e.target.classList.contains('nav__link')) return;
+  
+  const links = e.target.closest('.nav')
+                 .querySelectorAll('.nav__link')
+  const logo = e.target.closest('.nav')
+                .querySelector('img')
+  
+  links.forEach(l => { if (l !== e.target) l.style.opacity = `${this}` })
+  logo.style.opacity = `${this}`;
+}
+
+nav.addEventListener('mouseover', handleHover.bind(.75));
+nav.addEventListener('mouseout', handleHover.bind(1))
+
